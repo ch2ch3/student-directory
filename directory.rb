22f285787ed527d43531b4c55333902b6c1acf21
ETHEL NG <ethel.ng@gmail.com>
2014-08-06 10:32:52 +0100

    completed exercises 2–8, 10, 13

--- directory.rb
+++ directory.rb
@@ -1,6 +1,6 @@
 def input_students
-	puts "Please enter the names of the students"
-	puts "To finish, just hit return twice"
+	print "Please enter the names and cohorts of the students.\n"
+	puts "To finish, just hit return twice."
 	#create an empty array
 	students = []
 	#get the first name
@@ -8,33 +8,99 @@ def input_students
 	#while the name is not empty, repeat this code
 	while !name.empty? do
 		#add the student hash to the array
-		students << {:name => name, :cohort => :october}
-		puts "Now we have #{students.length} students"
+		puts "What cohort is #{name} in?"
+		cohort = gets.chomp.to_sym
+		if cohort.empty?
+			puts "Please enter a cohort, or press return to fill in the default value of August."
+			cohort = gets.chomp.to_sym
+			if cohort.empty?
+				cohort = :august
+			end
+		end
+
+		puts "What is #{name}'s country of birth?"
+		country = gets.chomp
+
+		students << {:name => name, :cohort => cohort, :country => country}
+
+		if students.length  == 1
+			puts "Now we have #{students.length} student. Enter another name."
+		else
+			puts "Now we have #{students.length} students. Enter another name."
+		end
 		#get another name from the user
 		name = gets.chomp
-	end 
-
+	end
 	# return array of students
 	students
-
 end
 
 def print_header
 	puts "The students of my cohort at Makers Academy"
-	puts "-------------------------"
+	puts "==========================================="
 end
 
-def print(students)
-	students.each do |student|
-		puts "#{student[:name]} (#{student[:cohort]} cohort)"
+def show(students)
+	students.each_with_index do |student, index|
+		puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort) from #{student[:country]}".center(43)
 	end
 end
 
+=begin # rewritten with while
+def show(students)
+	m = 0
+	while m < students.length
+		puts "#{students[m][:name]} (#{students[m][:cohort]} cohort)".center(50)
+		m += 1
+	end
+end
+=end
+
+=begin # rewritten with until
+def show(students)
+	m = 0
+	until m == students.length
+		puts "#{students[m][:name]} (#{students[m][:cohort]} cohort)".center(50)
+		m += 1
+	end
+end
+=end
+
+=begin # prints if first letter is A
+def print(students)
+	students.each_with_index do |student, index|
+		if student[:name][0].downcase == "a"
+			puts "#{index+1} #{student[:name]} (#{student[:cohort]} cohort)"
+		end
+	end
+end
+=end
+
+=begin # prints if name is less than 12 characters
+def print(students)
+	students.each_with_index do |student, index|
+		if student[:name][0].downcase == "a"
+			puts "#{index+1} #{student[:name]} (#{student[:cohort]} cohort)"
+		end
+	end
+end
+=end
+
 def print_footer(students)
-	puts "Overall, we have #{students.length} great students."
+	if students.length  == 1
+		puts "Overall, we have #{students.length} great student."
+	else
+		puts "Overall, we have #{students.length} great students."
+	end
 end
 
 students = input_students
 print_header
-print(students)
+
+if students.length > 1
+	show(students)
+else
+	print "There are not enough students so you don't get to see the list.\n\n"
+end
+
 print_footer(students)
