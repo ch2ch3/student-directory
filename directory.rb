ddcaef82782cd926b6a0678477c1daa20edcb9b2
ETHEL NG <ethel.ng@gmail.com>
2014-08-06 19:22:26 +0100

    Refactored code, edited prompts for clarity.

--- directory.rb
+++ directory.rb
@@ -1,30 +1,41 @@
+@students = []
+
+def print_menu
+	puts "Enter the menu number of the function you want to access."
+	puts "1. Input the students"
+	puts "2. Show the students"
+	puts "9. Exit"
+end
+
+def show_students
+	print_header
+	print_students_list
+	print_footer
+end
+
+def process(selection)		
+	case selection
+		when "1"
+			input_students
+		when "2"
+			show_students
+		when "9"
+			exit
+		else
+			puts "Whaa? Try again."
+	end
+end
+
 def interactive_menu
-	students = []
 	loop do
-		puts "1. Input the students"
-		puts "2. Show the students"
-		puts "9. Exit"
-		selection = gets.chomp
-		case selection
-			when "1"
-				students = input_students
-			when "2"
-				print_header
-				display_by_cohort(students)
-				print_footer(students)
-			when "9"
-				exit
-			else
-				puts "Huh? Try again."
-		end
+		print_menu
+		process(gets.chomp)
 	end
 end
 
 def input_students
 	puts "Please enter the details of the students."
 	puts "To finish, just hit return twice."
-	# create an empty array
-	students = []
 	# get the first name
 	puts "What is this student's name?"
 	# EXERCISE 11: uses chop in place of chomp
@@ -40,7 +51,7 @@ def input_students
 		
 		# EXERCISE 8: corrects for typos
 		while !calendar.include?(cohort) do
-			puts "Please enter a valid calendar month, or press return for a default value of August."
+			puts "Oops! Enter a valid calendar month, or press return for a default value of August."
 			cohort = gets.chomp.downcase.to_sym
 			# EXERCISE 8: supplies a default value of August if the value is still empty
 			if cohort.empty?
@@ -60,12 +71,12 @@ def input_students
 
 		# prevents the user from entering a string as height (if a string is entered, it is converted to 0)
 		while height == 0
-			puts "Please enter a valid number."
+			puts "Oops! Please enter a valid number."
 			height = gets.chomp.to_i
 		end
 
 		# add the student hash to the array
-		students << {	:name => name,
+		@students << {	:name => name,
 						:cohort => cohort,
 						:hobby => hobby,
 						:country => country,
@@ -73,16 +84,16 @@ def input_students
 					}
 
 		# EXERCISE 10: uses singular and plural form as appropriate
-		if students.length  == 1
-			puts "Now we have #{students.length} student. Enter another name."
+		if @students.length  == 1
+			puts "Now we have #{@students.length} student. Enter another name, or press return to exit."
 		else
-			puts "Now we have #{students.length} students. Enter another name."
+			puts "Now we have #{@students.length} students. Enter another name, or press return to exit."
 		end
 		#get another name from the user
 		name = gets.chomp
 	end
 	# return array of students
-	students
+	@students
 end
 
 def print_header
@@ -92,12 +103,12 @@ def print_header
 	puts "Name | Cohort | Hobby | Country | Height (cm)\n".center(70)
 end
 
-def display_by_cohort(students)
+def print_students_list
 	# EXERCISE 9: sorts the students array by an element in the hashes
-	students.sort_by! { |s| s[:cohort] }
+	@students.sort_by! { |s| s[:cohort] }
 
-	if students.length >= 1 # EXERCISE 13: only prints the list if there is at least 1 student
-		students.each_with_index do |student, index| # EXERCISE 2: adds an index number before each student
+	if @students.length >= 1 # EXERCISE 13: only prints the list if there is at least 1 student
+		@students.each_with_index do |student, index| # EXERCISE 2: adds an index number before each student
 			# EXERCISE 6: adds student country | EXERCISE 7: centers the output using the length of the header
 			puts "#{index+1}. #{student[:name].capitalize} | #{student[:cohort].capitalize} | #{student[:hobby].capitalize} | #{student[:country].capitalize} | #{student[:height]}".center(70)
 		end
@@ -106,16 +117,18 @@ def display_by_cohort(students)
 	end
 end
 
-def print_footer(students)
+def print_footer
 	puts
 	# EXERCISE 10: uses singular and plural form as appropriate
-	if students.length  == 1
-		puts "Overall, we have #{students.length} great student.".center(70)
+	if @students.length  == 1
+		puts "Overall, we have #{@students.length} great student.".center(70)
 	else
-		puts "Overall, we have #{students.length} great students.".center(70)
+		puts "Overall, we have #{@students.length} great students.".center(70)
 	end
 	puts
 end
+
+interactive_menu
 
 # EXERCISE 1
 #
@@ -164,10 +177,3 @@ end
 #		m += 1
 #	end
 # end
-
-# students = input_students
-# print_header
-# display_by_cohort(students)
-# print_footer(students)
-
-interactive_menu
